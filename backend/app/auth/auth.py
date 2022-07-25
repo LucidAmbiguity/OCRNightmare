d2059eb69a60cc24d44e7f4b4147d1a001f477d8
LucidAmbiguity <89605070+LucidAmbiguity@users.noreply.github.com>
2022-07-25 18:59:35 -0400

    test response shape

--- backend/app/auth/auth.py
+++ backend/app/auth/auth.py
@@ -7,4 +7,19 @@ from app.auth import auth  # pylint: disable=import-self
 @auth.route("", methods=['GET', 'POST'])
 def auth_root():
     """Root route of App Module"""
-    return {'auth':'auth root'}
+    return {
+            "code": 200, 
+            "messages": [
+                {
+                "code": "A00001", 
+                "text": "Success."
+                }
+            ], 
+            "result": {
+                "links": {
+                "login": "login", 
+                "register": "register"
+                }
+            }, 
+            "status": "ok"
+            }, 200, {'WWW.Authentication': 'Basic realm: "login required"'}
--- backend/tests/feature/routes/test_auth_route.py
+++ backend/tests/feature/routes/test_auth_route.py
@@ -18,10 +18,37 @@ def test_auth_page_get(test_client):
 def test_auth_page_get_returns_json(test_client):
     """
     GIVEN a Flask application
-    WHEN the '/' page is posted to (GET)
-    THEN check that a '405' status code is returned
+    WHEN the '/' page is requested to (GET)
+    THEN check that a json is returned
 
     """
 
-    response = test_client.post('/')
+    response = test_client.post('/auth')
     assert response.json is not None
+
+def test_auth_page_get_response_shape(test_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/' page is posted to (GET)
+    THEN check shape of returned response
+
+    """
+
+    response = test_client.get('/auth')
+    
+
+    assert 'code' in response.json.keys()
+    assert 'messages' in response.json.keys()
+    assert 'result' in response.json.keys()
+    assert 'status' in response.json.keys()
+    
+    assert 'code' in response.json['messages'][0].keys()
+    assert 'text' in response.json['messages'][0].keys()
+
+    assert 'links' in response.json['result'].keys()
+
+    assert 'login' in response.json['result']['links'].keys()
+    assert 'register' in response.json['result']['links'].keys()
+
+
+
