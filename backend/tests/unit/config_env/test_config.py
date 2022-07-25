f2450ade66e599a460815f7cf989959a38200b6f
LucidAmbiguity <89605070+LucidAmbiguity@users.noreply.github.com>
2022-07-25 16:30:01 -0400

    clean up

--- backend/tests/unit/config_env/test_config.py
+++ backend/tests/unit/config_env/test_config.py
@@ -1,5 +1,6 @@
 """test"""
 from config import Config
+from app import create_app
 
 def test_config_class_exists_and_has_expected_attributes():
     """
@@ -10,27 +11,11 @@ def test_config_class_exists_and_has_expected_attributes():
         basic config and setup for a Flask Factory app
         with DB
     """
-
     assert hasattr(Config,'FLASK_APP')
     assert hasattr(Config,'SECRET_KEY')
     assert hasattr(Config,'SQLALCHEMY_DATABASE_URI')
     assert hasattr(Config,'SQLALCHEMY_TRACK_MODIFICATIONS')
   # ['FLASK_APP', 'SECRET_KEY', 'SQLALCHEMY_DATABASE_URI', 'SQLALCHEMY_TRACK_MODIFICATIONS']
 
-# def test_config_class_exists_and_where():
-#     """
-#     Test config class exists and where
 
-#     In a perfect world this test will be commented out
-#     it purpose is to find where config is loading from
-#     as !existence will throw [ModuleNotFoundError: No module named 'config']
-#     however a stray bad path in your sys.path may cause files to import from the void.
 
-#     if you suspect such a thing uncomment this test 
-#         it will fail forcing output of the print to show where
-#         in the void it came from
-#     """
-
-#     import config # pylint:disable=import-outside-toplevel
-#     print(config.__file__)
-#     assert False
