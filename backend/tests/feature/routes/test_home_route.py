410a18de98f4b81c82c12d2fef2d50662bb26623
LucidAmbiguity <89605070+LucidAmbiguity@users.noreply.github.com>
2022-07-25 17:12:03 -0400

    test_home_page_get_returns_json

--- backend/tests/feature/routes/test_home_route.py
+++ backend/tests/feature/routes/test_home_route.py
@@ -52,5 +52,17 @@ def test_home_page_post_returns_json(test_client):
     response = test_client.post('/')
     assert response.json is not None
 
+def test_home_page_get_returns_json(test_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/' page is posted to (POST)
+    THEN check that a '405' status code is returned
+
+    """
+
+
+    response = test_client.get('/')
+    assert response.json is not None
+
 
             
